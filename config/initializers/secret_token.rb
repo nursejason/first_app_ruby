292b8d4e7f755e82b7e89480dae8e8bfc9066294
Jarmo <nurse.jason@gmail.com>
2013-10-05 23:53:03 -0400

    Initial push of files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FirstApp::Application.config.secret_key_base = 'ade5c15e19d86635cda1ef7484430d0e7e126ce7d72c7f0c92ed317a1a4da892b0d77e532e29ae2776e83c25298b9f9aec216113e8bc1af8c380222619b8e8e6'
---
+++ vendor/bundle/gems/sqlite3-1.3.8/ext/sqlite3/siteconf20131002-3672-hi9o0m.rb
@@ -0,0 +1,6 @@
+require 'rbconfig'
+dest_path = "./.gem.20131002-3672-9xv0zn"
+RbConfig::MAKEFILE_CONFIG['sitearchdir'] = dest_path
+RbConfig::CONFIG['sitearchdir'] = dest_path
+RbConfig::MAKEFILE_CONFIG['sitelibdir'] = dest_path
+RbConfig::CONFIG['sitelibdir'] = dest_path
